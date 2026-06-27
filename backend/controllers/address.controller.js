3b25155ec6f2c577b4417b59b35a30db379143c1
Prakash Verma <prakashverma861@gmail.com>
2026-06-27 11:15:30 +0530

    backend code completed

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -1,10 +1,39 @@
+import Address from "../models/addresssModel.js";
 
 
 // add address : /api/address/add
 export const addAddress = async (req, res)=>{
     try {
+
+        const userId = req.userId;
+        const {address} = req.body;
+
+        await  Address.create({
+            ...address,
+            userId,
+        });
+
+        res.status(201).json({message : "Address added successfully", success : true});
+
         
     } catch (error) {
+        console.error("Error adding address:", error);
+        res.status(500).json({ message: "Internal server error" });
+
         
+    }
+}
+
+// get any user address : /api/address/get
+export const getAddress = async (req, res)=>{
+    try {
+        const userId = req.userId; //as there will be multiple users address stored.
+        const address = await Address.find({userId}).sort({ createdAt : -1});
+
+    } catch (error) {
+        
+        console.error("Error fetching address:", error);
+        res.status(500).json({ message: "Internal server error" });
+
     }
 }
--- backend/routes/address.routes.js
+++ backend/routes/address.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { authUser } from "../middlewares/authUser.js";
+import { addAddress, getAddress } from "../controllers/address.controller.js";
 
 
 
 
 const addressRoutes = express.Router();
-
-
+// To create a new user address -
+addressRoutes.post("/add", authUser, addAddress );
+//To get any user address -
+addressRoutes.get("/get", authUser, getAddress );
 
 
 
